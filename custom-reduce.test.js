26748e9d323a0fee31d3138e592bf87d92114249
Michał Strzoda <michal.strzoda@gmail.com>
2024-01-16 09:21:08 +0100

    One more test to Task #2

--- custom-reduce.test.js
+++ custom-reduce.test.js
@@ -3,3 +3,7 @@ const customReduce = require('./custom-reduce');
 test('customReduce 1', () => {
     expect(customReduce([1, 2, 3], (acc, next) => acc + next, 10)).toBe(16);
 })
+
+test('customReduce 2', () => {
+    expect(customReduce([], (acc, next) => acc + next, 10)).toBe(10);
+})
